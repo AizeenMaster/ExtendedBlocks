bf365625082c91bfb88eb72bcc5f51687ab788c5
AizeenMaster <grasiasuriel@gmail.com>
2026-08-02 21:26:54 -0600

    Update NetheriteBlock.php

--- src/JavierLeon9966/ExtendedBlocks/block/NetheriteBlock.php
+++ src/JavierLeon9966/ExtendedBlocks/block/NetheriteBlock.php
@@ -1,1 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JavierLeon9966\ExtendedBlocks\block;
+
+use pocketmine\block\{Solid, BlockToolType};
+use pocketmine\item\TieredTool;
+
+class NetheriteBlock extends Solid
+{
+	use PlaceholderTrait;
+
+	protected $id = 525;
+
+	public function __construct(int $meta = 0)
+	{
+		parent::__construct($this->id, $meta, $this->getName());
+	}
+
+	public function getName(): string
+	{
+		return "Netherite Block";
+	}
+
+	public function getToolType(): int
+	{
+		return BlockToolType::TYPE_PICKAXE;
+	}
+
+	public function getToolHarvestLevel(): int
+	{
+		return TieredTool::TIER_DIAMOND;
+	}
+
+	public function getHardness(): float
+	{
+		return 50;
+	}
+
+	public function getBlastResistance(): float
+	{
+		return 6000;
+	}
+}
 
